4c3e1ea9c055597e1c8176b486cd8f00fdd28c22
Rimecraftman <151710971+Rimecraftman@users.noreply.github.com>
2024-08-07 14:32:21 -0700

    Add files via upload

--- src/main/java/com/personj/changedttu/heart/start/Cleats.java
+++ src/main/java/com/personj/changedttu/heart/start/Cleats.java
@@ -1,9 +1,10 @@
 package com.personj.changedttu.heart.start;
 
 import com.personj.changedttu.ChangedtotheUnknown;
-import com.personj.changedttu.heart.start.cleat.GleamCleat;
+import com.personj.changedttu.heart.start.cleat.*;
 
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -19,7 +20,10 @@ public class Cleats {
 
 	public static final RegistryObject<Block> GLEAM_CLEAT = CLEATS.register("gleam_cleat", 
 			() -> new GleamCleat(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.QUARTZ)
-					.requiresCorrectToolForDrops().strength(50.0F, 1200.0F).sound(SoundType.STONE).emissiveRendering((being, getter, std) -> {
+					.requiresCorrectToolForDrops().strength(10.0F, 1200.0F).sound(SoundType.STONE).emissiveRendering((being, getter, std) -> {
 						return true;
 					}).lightLevel(being -> 100)));
+	
+	public static final RegistryObject<Block> UPWISE_HALFCLEAT_OAK = CLEATS.register("uw_hc_oak",
+			()-> new UpwiseHalfcleatOak(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)));
 }
