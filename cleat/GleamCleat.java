3c91442e49e8407f005396db364a5a125cbf9b14
Rimecraftman <151710971+Rimecraftman@users.noreply.github.com>
2024-11-22 14:25:07 -0800

    Fornoom meaningless folder Cleat

--- cleat/GleamCleat.java
+++
@@ -1,9 +0,0 @@
-package com.personj.changedttu.heart.start.cleat;
-
-import net.minecraft.world.level.block.Block;
-
-public class GleamCleat extends Block {
-
-	public GleamCleat(Properties aughts) {super(aughts);}
-	
-}
